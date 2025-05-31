f99646ac4b2864f289378c08d87172563f641b1f
Thiago Sano <thiago.sano@outlook.com>
2025-05-30 22:55:38 -0300

    initial commit

---
+++ src/main/java/br/com/rocketseat/apicourse/ApicourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.apicourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rocketseat/apicourse/ApicourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.apicourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
